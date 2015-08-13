943ac190bdba9699f0ec1063b937df21386270a1
HUANG Manutea <huang.manutea@gmail.com>
2015-08-13 13:09:52 -1000

    cleaning the global scope + beginning the rectangle implementation
    todo: main js draw rectangle

--- js/shape.js
+++ js/shape.js
@@ -1,11 +1,16 @@
 var circleShape = new Circularlist("RIGHT_DOWN","LEFT_DOWN","LEFT_UP","RIGHT_UP");
+var rectShape = new Circularlist("HORIZONTAL","VERTICAL");
 var boundsX;
 var boundsY;
 
-//return String
+//return the type of the tangent as String
 function getType(dx,dy) {
 	var result;
-    if (dx > 0 && dy < 0) {
+    if ((Math.abs(dy)/Math.abs(dx))>50){
+        result = "VERTICAL";
+    } else if ((Math.abs(dx)/Math.abs(dy))>50){
+        result = "HORIZONTAL";
+    } else if (dx > 0 && dy < 0) {
         result ="RIGHT_DOWN";
     } else if (dx < 0 && dy < 0) {
         result = "LEFT_DOWN";
@@ -13,86 +18,86 @@ function getType(dx,dy) {
         result = "LEFT_UP";
     } else if (dx > 0 && dy > 0) {
         result = "RIGHT_UP";
-    }
+    } 
     return result;
 }
 
-function isCircle(points) {
-    var result = false;
-    var shape = new Circularlist("RIGHT_DOWN","LEFT_DOWN","LEFT_UP","RIGHT_UP");
-    var detected = [];
-    boundsX = new Circularlist();
-    boundsY = new Circularlist();
-    var step = 5;
-
-    var index = 0;
-    var currentX = clickX[0];
-    var currentY = clickY[0];
-    var type = null;
-
-    for (var i = step; i < points.length; i += step) {
-        var dx = clickX[i] - currentX;
-        var dy = -(clickY[i] - currentY);
-
-        if(dx === 0 || dy === 0) {
-            continue;
-        }
-
-        var newType = getType(dx, dy);
-        if(type === null || type != newType){
-            if(!circleShape.isNear(type,newType)) {
-                break;
-            }
-            boundsX[index] = currentX;
-            boundsY[index] = currentY;
-            detected[index++] = newType;
-        }
-        type = newType;
-        currentX = clickX[i];
-        currentY = clickY[i];
-
-        if (index >= shape.length()) {
-            result = true;
-            break;
-        }
-    }
-
-    return result;
-}
-
-function isCirc(arraypoints){
-    var result = false;
-    var stocktype = null;
-    var step = 10;
-    var curX = clickX[0];
-    var curY = clickY[0];
-    var index = 0; //index = 0, 1, 2, 3 
-    boundsX = new Circularlist();
-    boundsY = new Circularlist();
+function isRect(arraypoints){
+    var result = false; //we will return result, it is also used as a loop condition
+    var stocktype = null; //type of the previous tangent
+    var step = 10; //every <step> point
+    var curX = clickX[0]; //x value of current point, used to calculate dx
+    var curY = clickY[0]; //y value of current point, used to calculate dy
+    var index = 0; //index = 0, 1, 2, 3 : bounds iterator
+    boundsX = new Circularlist(); //x values of noticeable points: NORTH-EAST, NORTH-WEST, SOUTH-EAST, SOUTH-WEST
+    boundsY = new Circularlist();//y values of noticeable points: NORTH-EAST, NORTH-WEST, SOUTH-EAST, SOUTH-WEST
+    //loop through the arraypoints while there isn't any result
     for(var i=step;i<arraypoints.length&&!result;i=i+step){
-
+        //calculate dx and dy to find the tangeant type
         var dx = clickX[i]-curX;
         var dy = clickY[i]-curY;
-
-        if(!(dx===0 || dy===0)){
-
-            var curtype = getType(dx,dy);
-
-            if(stocktype===null){ //init we stock
+        //not a vertical nor horizontal line
+        var curtype = getType(dx,dy); //current type
+        //operate only if we have straight horizontal or vertical lines
+        if(curtype =="HORIZONTAL" ||curtype =="VERTICAL"){
+            if(stocktype===null){ //init we stock the bounds points
                 boundsX.push(curX);
                 boundsY.push(curY);
                 index++;
             }else{
-                if(circleShape.isNear(stocktype,curtype)){ //near form so stock
-                    boundsX.push(curX);
+                if(rectShape.isNear(stocktype,curtype)){ //previous tangent and current tangent are next to each other
+                    boundsX.push(curX); //pick up this point because it's where the tangent shift
                     boundsY.push(curY);
                     index++;
                 }
             }
-            stocktype = curtype;
-            curX = clickX[i];
+            stocktype = curtype; //the current type of tangent become the previous
+            curX = clickX[i]; //updating
             curY = clickY[i];
 
+            //if we have 4 types of tangent then it's true
+            if (index >= 4) {
+                result = true;
+            }
+        }
+    }
+    return result;
+}
+
+function isCirc(arraypoints){
+    var result = false; //we will return result, it is also used as a loop condition
+    var stocktype = null; //type of the previous tangent
+    var step = 10; //every <step> point
+    var curX = clickX[0]; //x value of current point, used to calculate dx
+    var curY = clickY[0]; //y value of current point, used to calculate dy
+    var index = 0; //index = 0, 1, 2, 3 : bounds iterator
+    boundsX = new Circularlist(); //x values of noticeable points: NORTH, SOUTH, EAST, WEST
+    boundsY = new Circularlist();//y values of noticeable points: NORTH, SOUTH, EAST, WEST
+    //loop through the arraypoints while there isn't any result
+    for(var i=step;i<arraypoints.length&&!result;i=i+step){
+        //calculate dx and dy to find the tangeant type
+        var dx = clickX[i]-curX;
+        var dy = clickY[i]-curY;
+        //not a vertical nor horizontal line
+        if(!(dx===0 || dy===0)){
+            var curtype = getType(dx,dy); //current type
+
+            if(stocktype===null){ //init we stock the bounds points
+                boundsX.push(curX);
+                boundsY.push(curY);
+                index++;
+            }else{
+                if(circleShape.isNear(stocktype,curtype)){ //previous tangent and current tangent are next to each other
+                    boundsX.push(curX); //pick up this point because it's where the tangent shift
+                    boundsY.push(curY);
+                    index++;
+                }
+            }
+            stocktype = curtype; //the current type of tangent become the previous
+            curX = clickX[i]; //updating
+            curY = clickY[i];
+
+            //if we have 4 types of tangent then it's true
             if (index >= 4) {
                 result = true;
             }
--- main.js
+++ main.js
@@ -21,13 +21,6 @@ if(typeof G_vmlCanvasManager != 'undefined') {
 }
 context = canvas.getContext("2d");
 
-var cX=0 ; //c = point from where we draw the circle
-var cY=0 ;
-var diamX = 0; //we will take the biggest
-var diamY = 0;
-var cD=0 ;//diameter of the circle
-var indexMaxY = 0;
-var indexMaxX = 0;
 var listShapes = [];
 clickX = [];
 clickY = [];
@@ -51,7 +44,7 @@ function addClick(x,y,dragging){
 
 function redraw(){
 	
-	context.strokeStyle = "#df4b26";
+	context.strokeStyle = "#008080";
 	context.lineJoin = "round";
 	context.lineWidth = 5;
 
@@ -75,32 +68,45 @@ function clean(){
 }
 
 function paintCircles(array){
-	for (var i = 0; i < listShapes.length; i++) {
-		context.beginPath();
-		context.arc(array[i].x,array[i].y,(array[i].d)/2,0*Math.PI,2*Math.PI);
-		context.closePath();
-		context.stroke();
+	for (var i = 0; i < array.length; i++) {
+		if(array[i] instanceof Circle){
+			paintCircle(array[i].x,array[i].y,array[i].d);
+		}else if(array[i] instanceof Rectangle){
+			paintRect(array[i].x,array[i].y,array[i].w,array[i].h);
+		}
 	}
+}
+
+function paintCircle(x,y,d){
+	context.beginPath();
+	context.arc(x,y,d/2,0*Math.PI,2*Math.PI);
+	context.closePath();
+	context.stroke();
+}
+function paintRect(x,y,w,h){
+	context.beginPath();
+	context.rect(x,y,w,h);
+	context.closePath();
+	context.stroke();
 }
 
 function decomposeArray(arrayX , arrayY){
 	var maxX= arrayX.get(0);
 	var maxY = arrayY.get(0);
-	indexMaxY = 0;
-	indexMaxX = 0;
+	var indexMax= {X : 0 , Y : 0}; //not a point, index values of the points with the x max and y max
 	for(var i=1;i<arrayX.length();i++){
 		if(arrayX.get(i)>maxX){
 			maxX = arrayX.get(i);
-			indexMaxX = i;
+			indexMax.X = i;
 		}
 	}
 	for(var j=1;j<arrayY.length();j++){
 		if(arrayY.get(j)>maxY){
 			maxY = arrayY.get(j);
-			indexMaxY = j;
+			indexMax.Y = j;
 		}
 	}
-
+	return indexMax;
 }
 
 //listeners
@@ -120,12 +126,21 @@ $('#canvas').mousemove(function(e){
 
 $('#canvas').mouseup(function(e){
 	paint=false;
+	if(isRect(clickX)){
+		var indexMax = decomposeArray(boundsX,boundsY); //we need the location of the max X and the max Y points, so we need their index
+
+	}
 	if(isCirc(clickX)){
-		decomposeArray(boundsX,boundsY);
-		cX = boundsX.get(indexMaxY);
-        cY = boundsY.get(indexMaxY);
-        diamX = Math.abs(boundsX.get(indexMaxX) - boundsX.get(indexMaxX-2));
-        diamY = Math.abs(boundsY.get(indexMaxY) - boundsY.get(indexMaxY-2));
+		var cX=0 ; //c = point from where we draw the circle
+		var cY=0 ;
+		var diamX = 0; //we will take the biggest
+		var diamY = 0;
+		var cD=0 ;//diameter of the circle
+		var indexMax = decomposeArray(boundsX,boundsY); //we need the location of the max X and the max Y points, so we need their index
+		cX = boundsX.get(indexMax.Y);
+        cY = boundsY.get(indexMax.Y);
+        diamX = Math.abs(boundsX.get(indexMax.X) - boundsX.get((indexMax.X)-2));
+        diamY = Math.abs(boundsY.get(indexMax.Y) - boundsY.get((indexMax.Y)-2));
         cD = Math.max(diamY,diamX);
         cY = cY - cD/2;
         listShapes.push(new Circle(cX,cY,cD));
